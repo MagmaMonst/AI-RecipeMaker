bdca868c6bb2eea0f840691d908a23f6b07f01db
MagmaMonst <123490972+MagmaMonst@users.noreply.github.com>
2024-12-03 23:20:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
-const port = process.env.PORT || 4000;
 
 const app = express();
 
